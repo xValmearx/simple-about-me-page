8ede049c8db93e8f70bde1ee6640642ac82bfeab
Caleb Taylor <calebs.phone2017@gmail.com>
2024-09-19 15:12:26 -0400

    created automative test for previous projects page to be found

--- pages/tests.py
+++ pages/tests.py
@@ -8,3 +8,12 @@ class HomePageTest(SimpleTestCase):
         responce = self.client.get("/")
 
         self.assertEqual(responce.status_code, 200)
+
+
+class PreviousProjectsTest(SimpleTestCase):
+    """Test for the home page of django project"""
+
+    def test_url_exist_at_current_location(self):
+        responce = self.client.get("/previous_projects/")
+
+        self.assertEqual(responce.status_code, 200)
