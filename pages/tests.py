a0a4fa96e36fd0fc94d6020ae83921a4bd610b0e
Caleb Taylor <calebs.phone2017@gmail.com>
2024-09-19 15:14:12 -0400

    created automative test for the contacts page to be found

--- pages/tests.py
+++ pages/tests.py
@@ -17,3 +17,12 @@ class PreviousProjectsTest(SimpleTestCase):
         responce = self.client.get("/previous_projects/")
 
         self.assertEqual(responce.status_code, 200)
+
+
+class ContactsTest(SimpleTestCase):
+    """Test for the home page of django project"""
+
+    def test_url_exist_at_current_location(self):
+        responce = self.client.get("/contacts/")
+
+        self.assertEqual(responce.status_code, 200)
