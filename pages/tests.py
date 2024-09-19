1a5c48ca012fbe0150367f507db3e159c1f2bc53
Caleb Taylor <calebs.phone2017@gmail.com>
2024-09-19 15:33:55 -0400

    created page name found test for each page

--- pages/tests.py
+++ pages/tests.py
@@ -1,22 +1,43 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 
 class HomePageTest(SimpleTestCase):
-    """Test for the home page of django project"""
+    """Test to see if page is found"""
 
     def test_url_exist_at_current_location(self):
         responce = self.client.get("/")
 
         self.assertEqual(responce.status_code, 200)
 
+    """test to see is page name is avalible """
+
+    def test_url_avalible_by_name(self):
+        responce = self.client.get(reverse("home"))
+
+        self.assertEqual(responce.status_code, 200)
+
+
+"""test for previous projects page"""
+
 
 class PreviousProjectsTest(SimpleTestCase):
-    """Test for the home page of django project"""
+    """test to see if page is found"""
 
     def test_url_exist_at_current_location(self):
         responce = self.client.get("/previous_projects/")
 
         self.assertEqual(responce.status_code, 200)
+
+    """test to see is page name is avalible """
+
+    def test_url_avalible_by_name(self):
+        responce = self.client.get(reverse("previous_projects"))
+
+        self.assertEqual(responce.status_code, 200)
+
+
+"""test for contacts page"""
 
 
 class ContactsTest(SimpleTestCase):
@@ -24,5 +45,12 @@ class ContactsTest(SimpleTestCase):
 
     def test_url_exist_at_current_location(self):
         responce = self.client.get("/contacts/")
+
+        self.assertEqual(responce.status_code, 200)
+
+    """test to see is page name is avalible """
+
+    def test_url_avalible_by_name(self):
+        responce = self.client.get(reverse("contacts"))
 
         self.assertEqual(responce.status_code, 200)
--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import HomePageView, PreviousProjectsView, ContactPageView
 
 urlpatterns = [
+    path("contacts/", ContactPageView.as_view(), name="contacts"),
     path("previous_projects/", PreviousProjectsView.as_view(), name="previous_projects"),
     path("", HomePageView.as_view(), name="home"),
-    path("/contacts", ContactPageView.as_view(), name="contacts"),
 ]
