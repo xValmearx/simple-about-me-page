015bcadf7421de580dba8fd32915692c6c0a9957
Caleb Taylor <calebs.phone2017@gmail.com>
2024-09-20 11:06:56 -0400

    commented all test class and methods in the test folder

--- pages/tests.py
+++ pages/tests.py
@@ -58,26 +58,42 @@ class HomePageTest(SimpleTestCase):
 class PreviousProjectsTest(SimpleTestCase):
     """test to see if page is found"""
 
+    """test to see if page will load"""
+
     def test_url_exist_at_current_location(self):
+        # gets client form the previous_projects page
         responce = self.client.get("/previous_projects/")
 
+        # test to see if the page is found
         self.assertEqual(responce.status_code, 200)
 
     """test to see is page name is avalible """
 
     def test_url_avalible_by_name(self):
+        # gets client from the previous_projects page
         responce = self.client.get(reverse("previous_projects"))
 
+        # checks if the name is avalible
         self.assertEqual(responce.status_code, 200)
 
+    """test to see if the page has the correct html"""
+
     def test_template_name_correct(self):
+
+        # gets client from the previous_projects page
         responce = self.client.get(reverse("previous_projects"))
 
+        # test to see if the previous_projects page is using the correct html
         self.assertTemplateUsed(responce, "previous_projects.html")
 
+    """test to see if the page has the correct header"""
+
     def test_template_contains(self):
+
+        # gets client form the previous_projejcts page
         response = self.client.get(reverse("previous_projects"))
 
+        # test to see if the page containes the proper header
         self.assertContains(response, "<h2>Previous projects I have worked on</h2>")
 
 
@@ -88,23 +104,37 @@ class ContactsTest(SimpleTestCase):
     """Test for the home page of django project"""
 
     def test_url_exist_at_current_location(self):
+
+        # creates client form the contacts page
         responce = self.client.get("/contacts/")
 
+        # test to see if page will load
         self.assertEqual(responce.status_code, 200)
 
     """test to see is page name is avalible """
 
     def test_url_avalible_by_name(self):
+
+        # creates client from the contacts page
         responce = self.client.get(reverse("contacts"))
 
+        # test to see if the page has the correct name
         self.assertEqual(responce.status_code, 200)
 
+    """test to see if the page has the correct html"""
+
     def test_template_name_correct(self):
+        # creates client from the contacts page
         responce = self.client.get(reverse("contacts"))
 
+        # test to see if the page has the correct html
         self.assertTemplateUsed(responce, "contacts.html")
 
+    """test to see if the page has the correct header"""
+
     def test_template_contains(self):
+        # gets client form the contacts page
         response = self.client.get(reverse("contacts"))
 
+        # test to see if the page has the propper header
         self.assertContains(response, "<h1>Contact Info</h1>")
