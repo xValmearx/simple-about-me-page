2345d07d1863e142a6e0542f3ca7520d7f7e92d1
Caleb Taylor <calebs.phone2017@gmail.com>
2024-09-20 01:45:49 -0400

    created new files and downloaded gunicorn to load project on heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-(a)%3oj33lp_ew7=a89!g3p1uraug4ph=xm16bopng__1tmle5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
